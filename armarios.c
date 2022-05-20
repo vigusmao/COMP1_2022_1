967113a34b993ce11be8b8f14db130b945000c1b
vigusmao <vigusmao@gmail.com>
2022-05-20 08:28:19 -0300

    armarios (LAB5)

---
+++ armarios.c
@@ -0,0 +1,33 @@
+/* 
+   Um vestiário possui n armários numerados de 1 a n.
+   Existem n crianças que farão a seguinte brincadeira.
+   Com os armários inicialmente fechados, a primeira criança
+   (criança 1) entrará no vestiário e abrirá todos os armários,
+   saindo do vestiário em seguida.
+   Depois a segunda criança (criança 2) entrará no vestiário e
+   mudará o estado (ou seja, se estiver aberto, fecha; se estiver
+   fechado, abre) de todos os armários pares.
+   A criança número 3 mudará o estado de todos os armários cujo número
+   é múltiplo de 3.
+   E assim por diante: a criança j, para todo j de 1 a n, 
+   entrará no vestiário e mudará o estado de todos os armários
+   que são múltiplos de n.
+   
+   PERGUNTA: no final, que armários estarão abertos?
+
+   1) Crie uma função "void armarios(int n)" para imprimir os armários
+      abertos no final.
+
+   2) No main(), peça o valor de n para o usuário e chame a função.
+*/
+
+#include <stdio.h>
+
+int main() {
+
+
+}
+
+
+
+
